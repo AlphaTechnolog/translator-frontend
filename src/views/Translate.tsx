cf0dc0f3e4002633cc2b87bc5df58aed43c4c8f1
Gabriel Guerra <gfranklings@gmail.com>
2023-04-13 18:40:22 -0400

    [chore] added more layout into the translate page

--- src/views/Translate.tsx
+++ src/views/Translate.tsx
@@ -1,10 +1,10 @@
 import type { Component } from 'solid-js'
-import { AiFillCopy, AiOutlineSearch } from 'solid-icons/ai'
+import { AiFillAudio, AiFillCopy, AiOutlineSearch } from 'solid-icons/ai'
 import { BiRegularTransferAlt } from 'solid-icons/bi'
 
 const Searchbar: Component = () => {
   return (
-    <button class="flex justify-start border-solid border-[1px] border-hover-background items-center bg-light-background rounded-xl py-2 px-4 cursor-pointer transition-all duration-500 hover:bg-lighter-background min-w-[40rem] shadow-sm hover:shadow-md">
+    <button class="flex justify-start items-center bg-light-background rounded-xl py-2 px-4 cursor-pointer transition-all duration-500 hover:bg-lighter-background min-w-[40rem] shadow-sm hover:shadow-md">
       <span class="text-primary text-lg"><AiOutlineSearch /></span>
       <span class="ml-2 text-surface-variant">Go to quick search (Ctrl + P)</span>
     </button>
@@ -19,13 +19,25 @@ const Translator: Component = () => {
         <span class="text-primary"><BiRegularTransferAlt /></span>
         <p>Spanish</p>
       </div>
-      <div class="rounded-xl bg-light-background p-10 px-5 pt-5 w-full h-[20rem] cursor-pointer flex flex-col justify-between items-stretch transition-all hover:bg-lighter-background hover:shadow-md shadow-sm duration-500">
-        <div class="min-h-full">
-          <textarea class="resize-none h-[95%] w-full bg-transparent placeholder-surface-variant text-on-primary-container outline-none text-2xl" placeholder="Enter something to translate..." />
-        </div>
-        <div class="pb-25 flex justify-end items-center">
+      <div class="rounded-xl bg-light-background p-10 px-5 pt-5 w-full h-[20rem] transition-all hover:bg-lighter-background hover:shadow-md shadow-sm duration-500">
+        <textarea class="resize-none h-[92%] w-full bg-transparent placeholder-surface-variant text-on-primary-container outline-none text-2xl" placeholder="Start typing to Get Started..." />
+        <div class="flex justify-end items-center space-x-2">
           <button class="mb-15 rounded-full bg-transparent transition-colors hover:bg-hover-background p-3 duration-500 cursor-pointer">
             <AiFillCopy />
+          </button>
+          <button class="mb-15 rounded-full bg-transparent transition-colors hover:bg-hover-background p-3 duration-500 cursor-pointer">
+            <AiFillAudio />
+          </button>
+        </div>
+      </div>
+      <div class="rounded-xl bg-light-background p-10 px-5 pt-5 w-full h-[20rem] shadow-sm">
+        <textarea disabled class="resize-none h-[92%] w-full bg-transparent placeholder-surface-variant text-on-primary-container outline-none text-2xl" placeholder="Translation will be here" />
+        <div class="flex justify-end items-center">
+          <button class="mb-15 rounded-full bg-transparent transition-colors hover:bg-hover-background p-3 duration-500 cursor-pointer">
+            <AiFillCopy />
+          </button>
+          <button class="mb-15 rounded-full bg-transparent transition-colors hover:bg-hover-background p-3 duration-500 cursor-pointer">
+            <AiFillAudio />
           </button>
         </div>
       </div>
