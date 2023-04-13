4d13c6f10831e15c2440fd9209a3d0a8d479c4c1
Gabriel Guerra <gfranklings@gmail.com>
2023-04-13 05:56:31 -0400

    [feat] no translation selected yet screen

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,26 @@
 import type { Component } from 'solid-js'
+import { TbLanguageOff } from 'solid-icons/tb'
 
+const Splash: Component = () => {
+  return (
+    <div class="flex justify-center items-center w-full h-full">
+      <div class="space-y-3">
+        <div class="flex justify-center items-center">
+          <span class="text-surface-variant text-[12rem]"><TbLanguageOff /></span>
+        </div>
+        <h1 class="font-bold text-center text-on-primary-container text-xl">
+          There is no translation selected yet
+        </h1>
+      </div>
+    </div>
+  )  
+}
+
+// <Home /> will be the no translation selected yet splash.
 export const Home: Component = () => {
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Splash />
     </>
   )
 }
